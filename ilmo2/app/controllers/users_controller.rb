d36101ab4cd5628e1e41ef0ab1f6e9e6f21d4a88
unknown <Noora@.(none)>
2009-11-18 21:40:50 +0200

    Kayttajan tietojen muokkaus lisatty

--- ilmo2/app/controllers/users_controller.rb
+++ ilmo2/app/controllers/users_controller.rb
@@ -16,4 +16,20 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Account updated"
+      redirect_to root_url
+    else
+      render :action => :edit
+    end
+  
+  end
+
 end
