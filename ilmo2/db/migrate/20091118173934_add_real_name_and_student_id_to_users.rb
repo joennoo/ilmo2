46610ff52e34e68f02c9994f0e4e675cdd5213fd
unknown <Noora@.(none)>
2009-11-20 23:54:22 +0200

    poistetaan hakemistorakenteen vanha haara

--- ilmo2/db/migrate/20091118173934_add_real_name_and_student_id_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddRealNameAndStudentIdToUsers < ActiveRecord::Migration
-  def self.up
-    add_column :users, :realname, :string
-    add_column :users, :studentid, :bigint
-  end
-
-  def self.down
-  end
-end
